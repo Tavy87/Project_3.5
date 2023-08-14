f6997495fe845c514f7e95596b2d98c1db567c6b
Tavy <tavysimchap@gmail.com>
2023-08-13 23:41:44 -0700

    second commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Widget(models.Model):
+    decription = models.CharField(max_length=250)
+    quanitity = models.IntegerField()
+    def __str__(self):
+        return self.decription
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Widget
 
 def index(request):
-    return HttpResponse("Wacky Widgets")
+    return HttpResponse('<h1>Wacky Widgets</h1>')
